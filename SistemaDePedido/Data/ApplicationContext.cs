f8d899c97d7bc10a28997c0a6e734f2dfda84c41
sarival <sarival@sarival.com.br>
2021-03-29 20:46:07 -0300

    Criando a classe de contexto

---
+++ SistemaDePedido/Data/ApplicationContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SistemaDePedido.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\mssqllocaldb; Initial Catalog=CursoEFCore; Integrated Security=True");
+        }
+    }
+}
