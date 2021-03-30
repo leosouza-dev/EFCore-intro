d7c5666f42023400cae1562b92b0f6077577e675
sarival <sarival@sarival.com.br>
2021-03-29 23:01:12 -0300

    Utilizando DataAnnotations

--- SistemaDePedido/Domain/Cliente.cs
+++ SistemaDePedido/Domain/Cliente.cs
@@ -1,13 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SistemaDePedido.Domain
 {
+    [Table("Clientes")]
     public class Cliente
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required]
         public string Nome { get; set; }
+
+        [Column("Phone")]
         public string Telefone { get; set; }
         public string CEP { get; set; }
         public string Estado { get; set; }
